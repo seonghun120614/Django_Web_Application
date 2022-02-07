422394fc926b570abf3b154270a960f8e7e07ffa
seonghun120614 <lnmin1206@gnu.ac.kr>
2022-02-07 13:41:09 +0000

    Create DB For Survey :) I have more to study to acquire this knowledge... U_U

--- survey/views.py
+++ survey/views.py
@@ -1,21 +1,37 @@
 from django.http import HttpResponse
 
-frame = f"""
+frame = """
+<!DOCTYPE html>
 <html>
-    <head lang="ko>
-        <meta charset="utf-8">
-        <meta name="author" content="Park Seonghun">
-        <meta name="description" content="My Schedule && My Diary && My Story. Top Topic is Machine Learning, Deep Learning, Computer programming, Math, Statics, ...">
-        <link rel="shortcut icon">
-        <title>별로의 놀이터</title>
-    </head>
-
+    <head lang="ko">{head}</head>
     <body>
-        <h1>Byeol_Lo's Play Ground</h1>
+        {view}
     </body>
 </html>
 """
 
+head = f"""
+        <meta charset="utf-8">
+        <meta name="author" content="Park Seonghun">
+        <meta name="description" content="My Schedule && My Diary && My Story. Top Topic is Machine Learning, Deep Learning, Computer programming, Math, Statics, ...">
+        <link rel="shortcut icon" href="/static/fabicon.ico">
+        <link rel="icon" href="/static/fabicon.ico">
+        <title>별로의 놀이터</title>
+"""
+
+views = {
+    "main" : f"""<h1> 안녕! 난 채뽓이야! :)</h1>
+        <h2>나랑 대화하자!</h2>
+        <form>
+            <p><textarea id="usr_chat"placeholder="봇이랑 채팅하기"></textarea></p>
+            <p><input type="submit" value="전송"/></p>
+        </form>
+        """
+}
+
+
 def index(request) :
-    global frame
-    return HttpResponse(frame)
+    
+    global frame, head, views
+
+    return HttpResponse(frame.format(head=head, view=views["main"]))
--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('survey/', include('survey.urls'))
+    path('survey/', include('survey.urls')),
+    path('admin/', admin.site.urls)
 ]
