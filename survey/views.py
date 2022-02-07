2466a9c2bea29240764e394db5029e48a35c45ad
seonghun120614 <lnmin1206@gnu.ac.kr>
2022-02-07 10:33:01 +0000

    i add the website basic frame

--- survey/views.py
+++ survey/views.py
@@ -1,4 +1,21 @@
 from django.http import HttpResponse
 
+frame = f"""
+<html>
+    <head lang="ko>
+        <meta charset="utf-8">
+        <meta name="author" content="Park Seonghun">
+        <meta name="description" content="My Schedule && My Diary && My Story. Top Topic is Machine Learning, Deep Learning, Computer programming, Math, Statics, ...">
+        <link rel="shortcut icon">
+        <title>별로의 놀이터</title>
+    </head>
+
+    <body>
+        <h1>Byeol_Lo's Play Ground</h1>
+    </body>
+</html>
+"""
+
 def index(request) :
-    return HttpResponse('Hello, world!')
+    global frame
+    return HttpResponse(frame)
