d4a8847d59e5bca920252cd6d6e7d745d5610a28
seonghun120614 <lnmin1206@gnu.ac.kr>
2022-02-09 11:01:23 +0000

    Making Auto remove DB

--- survey/models.py
+++ survey/models.py
@@ -13,6 +13,7 @@ class Question(models.Model) :
     
     def was_published_recently(self) :
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
     was_published_recently.admin_order_filed = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
@@ -39,7 +40,7 @@ class Test(models.Model):
     b = models.DateTimeField("date published")
 
 
-class TestDescade(models.Model) :
+class TestCascade(models.Model) :
 
     c = models.ForeignKey(Test, on_delete=models.CASCADE)
     d = models.CharField(max_length=100)
