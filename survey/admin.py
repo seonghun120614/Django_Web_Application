5fb1f3396c78beea110ce99defec20b7be8738f9
seonghun120614 <lnmin1206@gnu.ac.kr>
2022-02-09 13:26:09 +0000

    Start building DB in earnest

--- survey/admin.py
+++ survey/admin.py
@@ -2,35 +2,5 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-
-class ChoiceInline(admin.TabularInline) : # Tabular 형태로 보여주기
-    model = Choice  # 보여줄 모델 설정
-    extra = 3       # 몇 개 보여줄지 개수 설정
-
-
-class QuestionAdmin(admin.ModelAdmin) :
-    fieldsets = [
-        (
-            None,
-            {
-                'fields': ['question_text'],
-            }
-        ),
-        (
-            'Date information', 
-            {
-                'fields': ['pub_date'],
-                'classes': ['collapse'],
-            }
-        ),
-    ]
-
-    inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
-
-
-# admin 사이트에 Question을 추가하고,
-# QuestionAdmin을 추가함
-admin.site.register(Question, QuestionAdmin)
+admin.site.register(Question)
+admin.site.register(Choice)
--- survey/models.py
+++ survey/models.py
@@ -4,46 +4,21 @@ from django.db import models
 from django.utils import timezone
 
 class Question(models.Model) :
-    
-    question_text = models.CharField(max_length = 200)
-    pub_date = models.DateTimeField("date published")
-    
+
     def __str__(self) :
-        return self.question_text
-    
-    def was_published_recently(self) :
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    
-    was_published_recently.admin_order_filed = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
+        return self.question
 
+    def was_published_recently() :
+        return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
 
+    question = models.CharField(max_length = 100)
+    pub_date = models.DateTimeField("date published")
 
 class Choice(models.Model) :
 
-    # 👇🏻 ForeignKey는 각각의 Choice가 하나의 Question에 관계된다는 것을 Django에게 알려줌
-    question = models.ForeignKey(Question, on_delete = models.CASCADE)
-    choice_text = models.CharField(max_length = 200)
-    votes = models.IntegerField(default=0)
-    
     def __str__(self) :
-        return self.choice_text
-
-
-class Test(models.Model):
-
-    # 👇🏻 models.CharField는 max_length인자가 필요
-    a = models.CharField(max_length=10)
-
-    # 👇🏻 Question.pub_date에 한해서만 인간이 읽기 좋은 형식의 이름을 사용
-    b = models.DateTimeField("date published")
-
-
-class TestCascade(models.Model) :
-
-    c = models.ForeignKey(Test, on_delete=models.CASCADE)
-    d = models.CharField(max_length=100)
-
-    # 👇🏻 Integer Field를 생성해줌, 필수 인수는 아님 설정 안해줘도 상관무
-    e = models.IntegerField(default=0)
+        return self.answer
+    
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    answer = models.CharField(max_length = 200)
+    vote = models.IntegerField(default = 0)
